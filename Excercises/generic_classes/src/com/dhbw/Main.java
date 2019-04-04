7266aef6448dbde9a897b4aa69c70757c50b3c27
Lukas Abelt <lukas.abelt@airbus.com>
2019-04-04 22:23:18 +0200

    - Added stuff in main method for demonstration

--- Excercises/generic_classes/src/com/dhbw/Main.java
+++ Excercises/generic_classes/src/com/dhbw/Main.java
@@ -8,6 +8,16 @@ public class Main{
 
     public static void main(String[] args) {
         //KeyValuePair Demo
+        KeyValuePair<Integer, Double> pair1 = new KeyValuePair<>(-5,3.14159);
+        System.out.println("Key of pair1 is:" + pair1.getKey());
+        System.out.println("Value of pair1 is:" + pair1.getValue());
+        System.out.println("pair1 is: "+pair1);
+
+        pair1.setKey(42);
+        System.out.println("pair1 is: "+pair1);
+
+        pair1.setValue(2.7182818284590);
+        System.out.println("pair1 is: "+pair1);
 
         //GenericArrayWrapper Demo
 
@@ -20,11 +30,19 @@ public class Main{
         List<Integer> iList = Arrays.asList(1,2,3,4,5);
         List<Number> out = new ArrayList<>();
 
+        System.out.println("dlist is:");
         StaticListFunctions.printList(dList);
+        System.out.println("ilist is:");
         StaticListFunctions.printList(iList);
+        System.out.println("Sum of all elements in dList is:" + StaticListFunctions.getSum(dList));
+        System.out.println("Sum of all elements in iList is:" + StaticListFunctions.getSum(iList));
+        System.out.println("Combining dList and iList...");
         StaticListFunctions.combine(dList,iList,out);
+        System.out.println("Combined list is:");
         StaticListFunctions.printList(out);
+        System.out.println("Calculating elementwise sum of dList and iList...");
         StaticListFunctions.elementWiseSum(dList,iList,out);
+        System.out.println("Elementwise sums are:");
         StaticListFunctions.printList(out);
     }
 }
